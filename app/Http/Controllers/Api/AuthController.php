d5a3e355f9b5f38eb8ac0bca714425cd6aad76f9
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-05-02 19:42:23 +0800

    feat: connect register to backend

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use queue;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Resources\V1\UserResource;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\V1\StoreUserRequest;
+use App\Http\Requests\V1\UpdateUserRequest;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class AuthController extends Controller
+{
+    public function isAuthenticated()
+    {
+        return auth()->check();
+    }
+
+    public function createUser(StoreUserRequest $request)
+    {
+        try {
+            $user = User::create([
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'is_verified_student' => false
+            ]);
+
+            return response()->json([
+                'status' => 201,
+                'message' => 'User Created Successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ])->header('Access-Control-Allow-Origin', 'http://localhost:8000');
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 500,
+                'message' => $th->getMessage()
+            ]);
+        }
+    }
+
+    public function loginUser(Request $request)
+    {
+        try {
+            $validateUser = Validator::make($request->all(), 
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ]);
+            }
+
+            if(!Auth::attempt($request->only(['email', 'password']))){
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'Email & Password does not match with our record.',
+                ]);
+            }
+
+            if (Auth::attempt($request->only(['email', 'password']))) {
+                \Log::debug('User authenticated successfully');
+                \Log::debug(Auth::user());                
+            }
+            $user = Auth::user();
+            $request->session()->put('authenticated', time());
+            
+            return response()->json([
+                'status' => 204,
+                'message' => 'User Logged In Successfully',
+                'user' => $user->email,
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ])->header('Access-Control-Allow-Origin', 'http://localhost:8000');
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 500,
+                'message' => $th->getMessage()
+            ]);
+        }
+    }
+
+    public function logoutUser(Request $request)
+    {
+        try {
+            auth()->user()->tokens()->delete();
+
+            Session::flush();
+            Auth::logout(); 
+
+            return response()->json([
+                'status' => 204,
+                'message' => 'User Logged Out Successfully',
+            ]);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 500,
+                'message' => $th->getMessage()
+            ]);
+        }
+    }
+
+    public function updateUser(UpdateUserRequest $request)
+    {
+        try {
+            $user = auth()->user();
+            $user->update([
+                'name' => $request->name ?? $user->name,
+                'email' => $request->email ?? $user->email,
+            ]);
+
+            return response()->json([
+                'status' => 201,
+                'message' => 'Successfully updated user.',
+                'data' => new UserResource($user)
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Something went wrong.',
+                'data' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function changePassword(Request $request)
+    {
+        try {
+            $user = auth()->user();
+
+            $validateUser = Validator::make($request->all(), 
+            [
+                'currentPassword' => 'required',
+                'newPassword' => 'required'
+            ]);
+
+            if (Hash::check($request->currentPassword, $user->password)) { 
+                $user->update([
+                    'password' => Hash::make($request->newPassword)
+                ]);
+    
+                return response()->json([
+                    'status' => 201,
+                    'message' => 'Successfully updated user.',
+                    'data' => new UserResource($user)
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'Current password does not match with our record.',
+                ]);
+            }
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Something went wrong.',
+                'data' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function deleteUser()
+    {
+        try {
+            $user = auth()->user();
+            $user->delete();
+
+            return response()->json([
+                'status' => 204,
+                'message' => 'Successfully deleted user.',
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Something went wrong.',
+                'data' => $e->getMessage()
+            ]);
+        }
+    }
+}
