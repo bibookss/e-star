bd13219f05b6ea801059d7e11ea27293aed544b9
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-05-08 23:51:29 +0800

    fix: post resource date format

--- app/Http/Resources/V1/PostResource.php
+++ app/Http/Resources/V1/PostResource.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Resources\V1;
 
+use DateTime;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class PostResource extends JsonResource
 {
     public function toArray($request)
     {
+        $date = new DateTime($this->created_at);
+        $formattedDate = $date->format('M j, Y');
+
         return [
             'postId' => $this->id,
             'dormId' => $this->dorm_id,
             'userId' => $this->user_id,
-            'datePosted' => $this->created_at,
+            'datePosted' => $formattedDate,
             'review' => $this->review,
             'locationRating' => $this->location_rating,
             'securityRating' => $this->security_rating,
