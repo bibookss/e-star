dd1200ffe658dd54750bbd51d0db7978715fa57b
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-05-11 11:46:26 +0800

    fix: account

--- resources/views/dorms.blade.php
+++ resources/views/dorms.blade.php
@@ -6,24 +6,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="pt-3">
+<div class="container-fluid px-5 pt-3">
     <br><br>
-    <div class="">
+    <div class="row container-fluid">
       <form method="GET" action="{{ route('search') }}">
-        <div class="d-flex my-5 pt-3 justify-content-center" >
+        <div class="col my-5 d-flex justify-content-center pt-3">
             @csrf
-            <input type="text" class="address-input rounded-4 w-25 px-3" id="fname" name="q" placeholder="Enter address" value="<?php echo $_GET['q'] ?? ''; ?>">
-            <button class="ylw-btn text-white ms-2 py-3 px-2 fw-bold rounded-4 ms-2">Search now</button>
-      </form>  
-      <div class="d-flex">
-        @auth
-            <x-create-dorm></x-create-dorm>          
-        @endauth
-      </div>  
+            <input type="text" class="address-input rounded-4 w-25 mx-5 ps-3" id="fname" name="q" placeholder="Enter address" value="<?php echo $_GET['q'] ?? ''; ?>">
+            <button class="ylw-btn text-white px-4 py-2 fw-bold rounded-4">Search now</button>
+          </form>  
+          @auth
+          <x-create-dorm></x-create-dorm>          
+          @endauth
+        </div>
     </div>
 </div>
 
-<div class="d-flex gap-5 pt-3">
+<div class="d-flex">
     {{-- <h4 class="fw-bold px-3 pt-2 fs-2">Sort by:</h4>
     <div class="dropdown">
         <button class="dropdown-btn dropdown-btn2">Most Likes<i class="fa-solid fa-caret-down fa-lg ps-2"></i></button>
@@ -33,9 +32,9 @@
           <a href="#">Recent</a>
         </div>
     </div> --}}
-    <h4 class="fw-bold px-3 ms-5 pt-2 fs-2">Filter by:</h4>
+    <h4 class="fw-bold px-3 ms-5 pt-2 fs-2">Filter</h4>
     <div class="dropdown">
-        <button class="dropdown-btn dropdown-btn2">Ameneties<i class="fa-solid fa-caret-down fa-lg ps-2"></i></button>
+        <button class="dropdown-btn dropdown-btn2">by<i class="fa-solid fa-caret-down fa-lg ps-2"></i></button>
         <div class="dropdown-options">
           <div class="form-check form-switch">
             <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
@@ -58,44 +57,7 @@
             <label class="form-check-label" for="flexSwitchCheckDefault"><span class="ps-2">CCTV</span></label>
           </div>
         </div>
-    </div>
-    
-    <div class="dropdown">
-      <button class="dropdown-btn dropdown-btn2">Rating<i class="fa-solid fa-caret-down fa-lg "></i></button>
-      <div class="dropdown-options" style="width:170px;">
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-            <span class="ps-2">5 Stars</span>
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-            <span class="ps-2">4 Stars</span>
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-          <label class="form-check-label" for="flexRadioDefault2">
-            <span class="ps-2">3 Stars</span>
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-          <label class="form-check-label" for="flexRadioDefault2">
-            <span class="ps-2">2 Stars</span>
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-          <label class="form-check-label" for="flexRadioDefault2">
-            <span class="ps-2">1 Star</span>
-          </label>
-        </div>
       </div>
-    </div>
-    
   </div>
   <div class="d-flex flex-row flex-wrap gap-4 justify-content-center"  id="dorms-container">
       @foreach ($dorms['data'] as $dorm) 
@@ -116,6 +78,9 @@
     @endif
   </div>
 </form>
+
+
+
 
 
 <x-footer></x-footer>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,39 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<
-<div class="p-5 my-5 container row">
-    <div class="col-6">
-        <div class="fs-4 fw-bold pt-3">Account Settings</div>
-        <div class="py-5 ms-auto">
-            <div class="card justify-content-start" style="width:35rem;">
-                <div class="card-body row container">
-                    <span class="col-3 pt-2 ms-2 fw-bold">Email</span>
-                    <span class="col mt-2 fw-bold"> {{ $data['email'] }}</span>
-                </div>
-                <div class="row">
-                    <div class="col-2 py-3 mb-3 ms-1"></div>
-                    <button class="account-btn mx-auto col-4 fw-bold">Forgot Password?</button>
-                    <button class="account-btn mx-auto col-3 fw-bold text-danger">Delete Account</button>
+
+<div class="p-5 my-5 container">
+    <div class="row">
+        <div class="col-6">
+            <div class="fs-4 fw-bold pt-3">Account Settings</div>
+            <div class="py-5 ms-auto">
+                <div class="card justify-content-start" style="width:35rem;">
+                    <div class="card-body row container">
+                        <span class="col-3 pt-2 ms-2 fw-bold">Email</span>
+                        <span class="col mt-2 fw-bold"> {{ $data['email'] }}</span>
+                    </div>
+                    <div class="row">
+                        <div class="col-2 py-3 mb-3 ms-1"></div>
+                        <button class="account-btn mx-auto col-4 fw-bold">Forgot Password?</button>
+                        <button class="account-btn mx-auto col-3 fw-bold text-danger">Delete Account</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="col-2">
-        <div class="fs-4 fw-bold pb-5 pt-3">Your Reviews</div>
-        <div class="d-flex flex-column gap-5">
+        <div class="col-6">
+            <div class="fs-4 fw-bold pb-5 pt-3">Your Reviews</div>
             <div class="d-flex flex-column gap-5">
-                @foreach ($data['posts'] as $post) 
-                  <x-dorm-post :post="$post" />
-                @endforeach
+                <div class="d-flex flex-column gap-5">
+                    @foreach ($data['posts'] as $post) 
+                      <x-dorm-post :post="$post" />
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
 </div>
-<div class="d-flex justify-content-end py-3 px-5 me-3 mb-3">
-    <p class="col-4 fw-bold pt-4 ">Showing 2 of 87 reviews</p>
-    <button class="col-2 blue-btn text-white rounded-4">Load More</button>
-</div>
+
 <x-footer></x-footer>
 
 
